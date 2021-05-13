6a4e110926748a648d7285f4c5076d16a7ff256e
Bjørn Kåre Sæbø <bjornksa@stud.ntnu.no>
2021-05-13 15:18:54 +0200

    JUSTGO-10 Implement video thumbnails and previews [SB, ZM]

--- JustGo/app/src/main/java/com/example/justgo/PictureVideoAdapter.kt
+++ JustGo/app/src/main/java/com/example/justgo/PictureVideoAdapter.kt
@@ -1,23 +1,35 @@
 package com.example.justgo
 
 import android.content.Context
+import android.media.ThumbnailUtils
 import android.net.Uri
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
-import android.widget.TextView
 
 class PictureVideoAdapter(context : Context, private val dataSource: ArrayList<Uri>) : BaseAdapter() {
 
+    private val context = context
     private val inflater: LayoutInflater
             = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view = inflater.inflate(R.layout.list_item_picture_video, parent, false)
         val picture_video_view: ImageView = view.rootView.findViewById<ImageView>(R.id.picture_video_imageView)
-        picture_video_view.setImageURI(getItem(position))
+        val element = getItem(position) as Uri
+        if(element.toString().contains(".mp4"))
+        {
+            val thumbnail = ThumbnailUtils.createVideoThumbnail(element.path!!, MediaStore.Video.Thumbnails.MINI_KIND);
+            picture_video_view.setImageBitmap(thumbnail)
+        //.createVideoThumbnail(videoFile, 0, 0); //Creation of Thumbnail of video
+        }
+        else
+        {
+            picture_video_view.setImageURI(element)
+        }
         picture_video_view.setScaleType(ImageView.ScaleType.CENTER_CROP)
         return picture_video_view
     }
